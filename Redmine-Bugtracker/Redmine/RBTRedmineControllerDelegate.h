64976baa00f5a18f8e05cf8eb7bf0ac1e28e76d6
Demetrius Albuquerque <demetrius.albuquerque@tap4mobile.com.br>
2013-01-04 17:42:08 -0400

    commit inicial

---
+++ Redmine-Bugtracker/Redmine/RBTRedmineControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  RBTRedmineControllerDelegate.h
+//  Redmine-Bugtracker
+//
+//  Created by demetrius on 04/01/13.
+//  Copyright (c) 2013 demetrius albuquerque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RBTRedmineControllerDelegate : NSObject
+
+@end
---
+++ Redmine-Bugtracker/Redmine/RBTRedmineControllerDelegate.m
@@ -0,0 +1,13 @@
+//
+//  RBTRedmineControllerDelegate.m
+//  Redmine-Bugtracker
+//
+//  Created by demetrius on 04/01/13.
+//  Copyright (c) 2013 demetrius albuquerque. All rights reserved.
+//
+
+#import "RBTRedmineControllerDelegate.h"
+
+@implementation RBTRedmineControllerDelegate
+
+@end
