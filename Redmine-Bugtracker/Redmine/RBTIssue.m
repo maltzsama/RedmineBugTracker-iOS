a58861dc96cb81bca277147e634cad6a5e5b919d
Demetrius Albuquerque <demetrius.albuquerque@tap4mobile.com.br>
2013-01-09 17:20:31 -0400

    Change "System operation" for "Device model" tag

--- Redmine-Bugtracker/Redmine/RBTIssue.m
+++ Redmine-Bugtracker/Redmine/RBTIssue.m
@@ -10,7 +10,7 @@
 #import <sys/utsname.h>
 
 NSString *const RBTVersionSystem = @"System Version";
-NSString *const RBTOperationSystem = @"Operation System";
+NSString *const RBTDeviceModel = @"Device model";
 NSString *const RBTLanguageSystem = @"Language";
 NSString *const RBTCountrySystem = @"Country";
 NSString *const RBTAppVersionSystem = @"App Version";
@@ -58,19 +58,15 @@ NSString* machineName()
      @"iPhone5,2" on iPhone 5
      */
 
-    NSArray *modelTypeArray = [[NSArray alloc] initWithObjects:@"i386",@"iPod1,1", @"iPod2,1",@"iPod3,1",
-                                                                     @"iPod4,1", @"iPhone1,1", @"iPhone1,2", @"iPhone2,1", @"iPad1,1",@"iPad2,1",@"iPad3,1",@"iPhone3,1",
-                                                                     @"iPhone4,1",@"iPhone5,1",@"iPhone5,2",nil];
-    NSArray *modelNameArray = [[NSArray alloc] initWithObjects:@"simulator",@"iPod Touch",@"iPod Touch Second Generation",
-                                                                      @"iPod Touch Third Generation",@"iPod Touch Fourth Generation",@"iPhone",@"iPhone 3G",@"iPhone 3GS",@"iPad",
-                                                                      @"iPad 2",@"iPad 3", @"iPhone 4",@"iPhone 4S", @"iPhone 5", @"iPhone 5", nil];
+    NSArray *modelTypeArray = [[NSArray alloc] initWithObjects: @"i386",@"iPod1,1", @"iPod2,1", @"iPod3,1", @"iPod4,1", @"iPhone1,1", @"iPhone1,2", @"iPhone2,1", @"iPad1,1", @"iPad2,1", @"iPad3,1", @"iPhone3,1", @"iPhone4,1", @"iPhone5,1", @"iPhone5,2", nil];
+    NSArray *modelNameArray = [[NSArray alloc] initWithObjects: @"simulator", @"iPod Touch", @"iPod Touch Second Generation", @"iPod Touch Third Generation", @"iPod Touch Fourth Generation", @"iPhone", @"iPhone 3G", @"iPhone 3GS", @"iPad", @"iPad 2", @"iPad 3", @"iPhone 4", @"iPhone 4S", @"iPhone 5", @"iPhone 5", nil];
 
     struct utsname systemInfo;
     uname(&systemInfo);
     
     NSString *model = [NSString stringWithCString:systemInfo.machine
                               encoding:NSUTF8StringEncoding];
-    NSString *name = [[NSString alloc] init];
+    NSString *name = nil;
     if ([modelTypeArray containsObject:model]){
         name = [modelNameArray objectAtIndex:[modelTypeArray indexOfObject:model]];
         
@@ -98,7 +94,7 @@ NSString* machineName()
     NSMutableDictionary *customInfo = [[NSMutableDictionary alloc]init];
 
     [customInfo setObject:[[UIDevice currentDevice]systemVersion] forKey:RBTVersionSystem];
-    [customInfo setObject:machineName() forKey:RBTOperationSystem];
+    [customInfo setObject:machineName() forKey:RBTDeviceModel];
     [customInfo setObject:[languageArray objectAtIndex:0] forKey:RBTLanguageSystem];
     [customInfo setObject:country forKey:RBTCountrySystem];
     [customInfo setObject:appVersion forKey:RBTAppVersionSystem];
